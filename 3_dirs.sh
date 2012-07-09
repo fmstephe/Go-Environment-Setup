c99d98f0798830a443d2b8a3ae69ea82f75eb277
Francis Stephens <francisstephens@gmail.com>
2012-07-09 19:30:29 +0000

    Added my github path to GOROOT/src in 3_dirs.sh

--- 3_dirs.sh
+++ 3_dirs.sh
@@ -3,5 +3,7 @@
 mkdir ~/golang
 mkdir ~/golang/proj
 mkdir ~/golang/proj/src
+mkdir ~/golang/proj/src/github.com/
+mkdir ~/golang/proj/src/github.com/fmstephe
 mkdir ~/golang/proj/bin
 mkdir ~/golang/proj/pkg
