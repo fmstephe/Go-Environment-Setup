035ca122e73c712de78d7184f1daf68184bba4ea
Francis Stephens <francisstephens@gmail.com>
2012-10-30 21:18:07 +0000

    Added some git alias shortcuts

--- 2_env.sh
+++ 2_env.sh
@@ -14,4 +14,11 @@ echo alias psrc=\"cd \$GOPATH/src/github.com/fmstephe\"  >> $HOME/.bash_aliases
 echo alias pkg=\"cd \$GOPATH/pkg\"  >> $HOME/.bash_aliases
 echo alias gsrc=\"cd \$GOROOT/src/pkg\"  >> $HOME/.bash_aliases
 echo alias gi=goinstall  >> $HOME/.bash_aliases
-
+echo alias gst="git status"
+echo alias gco="git checkout"
+echo alias gcm="git commit"
+echo alias gbr="git branch"
+echo alias glg="git log"
+echo alias pprof="go tool pprof"
+echo alias bnc="go test -bench='.*'"
+echo alias ls="ls -1"
