f1ca4ebd691c3e95783b0bdee0bbedad9297845b
Francis Stephens <francisstephens@gmail.com>
2013-08-25 15:25:31 +0100

    Now we just copy our dot_files
    This makes things a bit simpler. Although not quite idempotent, multiple
    runs will overwrite the backups of your original dot files. I'm sure
    there is a much better way to do this.

--- 2_env.sh
+++ 2_env.sh
@@ -1,24 +1,12 @@
 #!/bin/sh
 
-echo export GOLANG=\$HOME/golang >> $HOME/.profile
-echo export GOROOT=\$GOLANG/go >> $HOME/.profile
-echo export GOARCH=amd64 >> $HOME/.profile
-echo export GOOS=linux >> $HOME/.profile
-echo export GOBIN=\$GOROOT/bin >> $HOME/.profile
-echo export GOPATH=\$GOLANG/proj >> $HOME/.profile
-echo PATH=\$PATH:\$GOPATH/bin:\$GOBIN >> $HOME/.profile
+cp $HOME/.bash_aliases $HOME/.bash_aliases_old
+cp $HOME/.profile $HOME/.profile_old
+cp $HOME/.vimrc $HOME/.vimrc_old
+cp $HOME/.gitconfig $HOME/.gitconfig_old
+cp .bash_aliases $HOME/
+cp .profile $HOME/
+cp .vimrc $HOME/
+cp .gitconfig $HOME/
 
-. ~/.profile
-
-echo alias psrc=\"cd \$GOPATH/src/github.com/fmstephe\" >> $HOME/.bash_aliases
-echo alias pkg=\"cd \$GOPATH/pkg\" >> $HOME/.bash_aliases
-echo alias gsrc=\"cd \$GOROOT/src/pkg\" >> $HOME/.bash_aliases
-echo alias gi=goinstall >> $HOME/.bash_aliases
-echo alias gst=\"git status\" >> $HOME/.bash_aliases
-echo alias gco=\"git checkout\" >> $HOME/.bash_aliases
-echo alias gcm=\"git commit\" >> $HOME/.bash_aliases
-echo alias gbr=\"git branch\" >> $HOME/.bash_aliases
-echo alias glg=\"git log\" >> $HOME/.bash_aliases
-echo alias pprof=\"go tool pprof\" >> $HOME/.bash_aliases
-echo alias bnc=\"go test -bench='.*'\" >> $HOME/.bash_aliases
-echo alias ls=\"ls -1\" >> $HOME/.bash_aliases
+echo "Please log out and log back into your system"
---
+++ 6_vim.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp $HOME/.vimrc $HOME/.vimrc_old
+cp .vimrc $HOME/
