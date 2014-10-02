f894ae4d7791a966a445864c7c19eda6a76c914e
Francis Stephens <francisstephens@gmail.com>
2014-10-02 17:37:52 +0100

    Don't backup dot-files if they don't exist

--- 2_env.sh
+++ 2_env.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 
-cp $HOME/.bash_aliases $HOME/.bash_aliases_old
-cp $HOME/.profile $HOME/.profile_old
-cp $HOME/.vimrc $HOME/.vimrc_old
-cp $HOME/.gitconfig $HOME/.gitconfig_old
+if [ -f $HOME/.bash_aliases ]
+then
+	mv $HOME/.bash_aliases $HOME/.bash_aliases_old
+fi
+if [ -f $HOME/.profile ]
+then
+	mv $HOME/.profile $HOME/.profile_old
+fi
+if [ -f $HOME/.vimrc ]
+then
+	mv $HOME/.vimrc $HOME/.vimrc_old
+fi
+if [ -f $HOME/.gitconfig ]
+then
+	mv $HOME/.gitconfig $HOME/.gitconfig_old
+fi
 cp .bash_aliases $HOME/
+. $HOME/.bash_aliases
 cp .profile $HOME/
+. $HOME/.profile
 cp .vimrc $HOME/
+. $HOME/.vimrc
 cp .gitconfig $HOME/
+. $HOME/.gitconfig
 
 echo "Please log out and log back into your system"
