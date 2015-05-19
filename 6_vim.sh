91950dd61dc5dd5dbb9a34eecb6cf0b8bafd8f2b
Francis Stephens <francisstephens@gmail.com>
2015-05-19 10:31:59 +0100

    Now installing vundle plugins as part of setup

--- 6_vim.sh
+++ 6_vim.sh
@@ -2,9 +2,15 @@
 
 if [ -f $HOME/.vimrc ]
 then
-	cp $HOME/.vimrc $HOME/.vimrc_old
+	mv $HOME/.vimrc $HOME/.vimrc_old
 fi
 cp .vimrc $HOME/
 
+if [ -d $HOME/.vim ]
+then
+	mv $HOME/.vim/ $HOME/.vim_old/
+fi
 mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+vim +PluginInstall +qall
