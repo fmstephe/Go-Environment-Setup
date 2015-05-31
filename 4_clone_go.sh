c1f1e259551e9f7e6f87ea6baa24dac576a1e64e
Francis Stephens <francisstephens@gmail.com>
2015-05-31 14:06:58 +0100

    Taking care to checkout release branch 1.4 before building go

--- 4_clone_go.sh
+++ 4_clone_go.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd $HOME/golang
-git clone https://go.googlecode.com/go
+git clone https://go.googlesource.com/go
 cd $HOME/golang/go/src
+git checkout release-branch.go1.4
 ./all.bash
