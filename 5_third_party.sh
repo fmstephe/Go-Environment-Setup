e86e6b2677d4fd7eebaa23924c395d45874afa9d
Francis Stephens <francisstephens@gmail.com>
2015-12-14 16:06:27 +0000

    Adding goimport to thirdparty go installs

--- 5_third_party.sh
+++ 5_third_party.sh
@@ -3,5 +3,8 @@
 go get github.com/skelterjohn/go-gb/gb
 go install github.com/skelterjohn/go-gb/gb
 
+go get golang.org/x/tools/cmd/goimports
+go install golang.org/x/tools/cmd/goimports
+
 source $HOME/.bash_aliases
 source $HOME/.profile
