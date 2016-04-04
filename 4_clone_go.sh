7c3ea66ae571ab3d6db728df910457358488e818
Francis Stephens <fmstephe@debianvm>
2016-04-04 11:39:35 -0500

    Now building go 1.6 with bootstrap

--- 4_clone_go.sh
+++ 4_clone_go.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Setup bootstrap go version
+wget https://storage.googleapis.com/golang/go1.4.3.linux-amd64.tar.gz
+tar xvfz go1.4.3.linux-amd64.tar.gz
+mv go ../go1.4
+rm go1.4.3.linux-amd64.tar.gz
+
 cd $HOME/golang
 git clone https://go.googlesource.com/go
 cd $HOME/golang/go/src
-git checkout release-branch.go1.4
+git checkout release-branch.go1.6
 ./all.bash
+
