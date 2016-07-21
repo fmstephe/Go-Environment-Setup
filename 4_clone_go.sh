f2b071c3854f39565efa101da0850b4c603811d8
Francis Stephens <francisstephens@gmail.com>
2016-07-21 17:27:13 +0200

    Clone Go downloads different go binaries for darwin/linux

--- 4_clone_go.sh
+++ 4_clone_go.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+if [[ $(uname) == "Darwin" ]]
+then
+	wget https://storage.googleapis.com/golang/go1.4.3.darwin-amd64.tar.gz
+	tar xvfz go1.4.3.darwin-amd64.tar.gz
+	mv go ../go1.4
+	rm go1.4.3.darwin-amd64.tar.gz
+else
+	wget https://storage.googleapis.com/golang/go1.4.3.linux-amd64.tar.gz
+	tar xvfz go1.4.3.linux-amd64.tar.gz
+	mv go ../go1.4
+	rm go1.4.3.linux-amd64.tar.gz
+fi
+
 # Setup bootstrap go version
-wget https://storage.googleapis.com/golang/go1.4.3.linux-amd64.tar.gz
-tar xvfz go1.4.3.linux-amd64.tar.gz
-mv go ../go1.4
-rm go1.4.3.linux-amd64.tar.gz
 
 cd $HOME/golang
 git clone https://go.googlesource.com/go
 cd $HOME/golang/go/src
-git checkout release-branch.go1.6
+git checkout release-branch.go1.7
 ./all.bash
-
