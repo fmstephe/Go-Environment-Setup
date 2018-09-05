c450eff6c503af7980a61402aabc4cc3b25b37a9
Francis Stephens <francisstephens@gmail.com>
2018-09-05 16:06:38 +0200

    Removing unused install

--- 5_third_party.sh
+++ 5_third_party.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-go get github.com/skelterjohn/go-gb/gb
-go install github.com/skelterjohn/go-gb/gb
-
 go get golang.org/x/tools/cmd/goimports
 go install golang.org/x/tools/cmd/goimports
 
