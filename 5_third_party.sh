75393cd5fff22307f1df2e5578a314a0c61d9328
Francis Stephens <francisstephens@gmail.com>
2018-09-05 17:12:20 +0200

    Fixing fzf install

--- 5_third_party.sh
+++ 5_third_party.sh
@@ -3,8 +3,8 @@
 go get golang.org/x/tools/cmd/goimports
 go install golang.org/x/tools/cmd/goimports
 
-go get github.com/junegunn/fzf/src/fzf
-go install github.com/junegunn/fzf/src/fzf
+go get github.com/junegunn/fzf
+go install github.com/junegunn/fzf
 
 source $HOME/.bash_aliases
 source $HOME/.profile
