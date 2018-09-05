34e0a66b522ed3fcd9dee109decc6313a8bce8ef
Francis Stephens <francis@adjust.com>
2018-09-05 16:50:22 +0200

    Providing fake 1.4 for OSX

--- 4_clone_go.sh
+++ 4_clone_go.sh
@@ -2,10 +2,10 @@
 
 if [[ $(uname) == "Darwin" ]]
 then
-	wget https://storage.googleapis.com/golang/go1.4.3.darwin-amd64.tar.gz
-	tar xvfz go1.4.3.darwin-amd64.tar.gz
+	wget https://storage.googleapis.com/golang/go1.8.3.darwin-amd64.tar.gz
+	tar xvfz go1.8.3.darwin-amd64.tar.gz
 	mv go ../go1.4
-	rm go1.4.3.darwin-amd64.tar.gz
+	rm go1.8.3.darwin-amd64.tar.gz
 else
 	wget https://storage.googleapis.com/golang/go1.4.3.linux-amd64.tar.gz
 	tar xvfz go1.4.3.linux-amd64.tar.gz
