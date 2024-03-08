ce4d0e64ae40957c850ee780c7465ba6c7cba355
Francis Stephens <francisstephens@gmail.com>
2024-03-08 10:29:48 +0000

    Adding tree and fzf to install dependencies

--- 1_install_dep.sh
+++ 1_install_dep.sh
@@ -5,3 +5,5 @@ sudo apt-get --assume-yes install git
 sudo apt-get --assume-yes install bison gawk gcc libc6-dev make
 sudo apt-get --assume-yes install graphviz
 sudo apt-get --assume-yes install libncurses5-dev
+sudo apt-get --assume-yes install tree
+sudo apt-get --assume-yes install fzf
