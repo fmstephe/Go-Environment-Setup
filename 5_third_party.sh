8e57234b28b686c6d0ae54d8493551e9dfe0254f
Francis Stephens <francisstephens@gmail.com>
2025-01-15 16:39:28 +1300

    Install gcassert
    This tool lets us assert that code compiles with specific
    characteristics like method inlining, stack allocation etc.

--- 5_third_party.sh
+++ 5_third_party.sh
@@ -7,6 +7,8 @@ go install golang.org/x/tools/cmd/goimports@latest
 
 go install github.com/junegunn/fzf@latest
 
+go install github.com/jordanlewis/gcassert/cmd/gcassert@latest
+
 source $HOME/.bash_aliases
 source $HOME/.profile
 
